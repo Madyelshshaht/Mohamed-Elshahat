30f2cf89bac48fffdd01d9b9481ded826a848027
Mady <madylabtop@gmail.com>
2025-06-29 19:08:51 +0300

    Filter Projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,61 +1,13 @@
-import React, { useMemo } from "react";
 
 import { motion } from 'framer-motion';
 
 import Heading from "../components/heading/Heading";
 
-// Cars
-import Cars from "../assets/Projects-img/Cars/cars.png";
-import cc1 from "../assets/Projects-img/Cars/cc1.png";
-import cc2 from "../assets/Projects-img/Cars/c22.png";
-import cc3 from "../assets/Projects-img/Cars/c33.png";
-import cc4 from "../assets/Projects-img/Cars/cc4.png";
-import cc5 from "../assets/Projects-img/Cars/cc5.png";
-import cc6 from "../assets/Projects-img/Cars/cc6.png";
-import cc7 from "../assets/Projects-img/Cars/cc7.png";
+
+import React, { useEffect, useState } from 'react'
 
 
-// Crypto-Img
-import Crypto from "../assets/Projects-img/Crypto-img/Crypto123.png";
-import c1 from "../assets/Projects-img/Crypto-img/c1.png";
-import c2 from "../assets/Projects-img/Crypto-img/c2.png";
-import c3 from "../assets/Projects-img/Crypto-img/c3.png";
-import c4 from "../assets/Projects-img/Crypto-img/c4.png";
-import c5 from "../assets/Projects-img/Crypto-img/c5.png";
-
-
-// TRAVEL
-
-import Travel from "../assets/Projects-img/travel-img/travel.png";
-import t1 from "../assets/Projects-img/travel-img/t1.png";
-import t2 from "../assets/Projects-img/travel-img/t2.png";
-import t3 from "../assets/Projects-img/travel-img/t3.png";
-import t4 from "../assets/Projects-img/travel-img/t4.png";
-import t5 from "../assets/Projects-img/travel-img/t5.png";
-import t6 from "../assets/Projects-img/travel-img/t6.png";
-
-// shief
-import Shief from "../assets/Projects-img/sheif/shief.png";
-import s2 from "../assets/Projects-img/sheif/s2.png";
-import s3 from "../assets/Projects-img/sheif/s3.png";
-import s4 from "../assets/Projects-img/sheif/s4.png";
-import s5 from "../assets/Projects-img/sheif/s5.png";
-import s6 from "../assets/Projects-img/sheif/s6.png";
-
-// wordpress Protfolio
-import porto1 from "../assets/Projects-img/Portofolio/porot1.png"
-import p1 from "../assets/Projects-img/Portofolio/p1.png"
-import p2 from "../assets/Projects-img/Portofolio/p2.png"
-import p3 from "../assets/Projects-img/Portofolio/3p.png"
-import p4 from "../assets/Projects-img/Portofolio/p4.png"
-import p5 from "../assets/Projects-img/Portofolio/p5.png"
-import p6 from "../assets/Projects-img/Portofolio/p6.png"
-import porto2 from "../assets/Projects-img/Portofolio/porot1.png"
-
-// XO
-
-import xo from "../assets/Projects-img/XO/XO.png";
-import win from "../assets/Projects-img/XO/win.png";
+import { ScaleLoader } from "react-spinners";
 
 
 import Project from "../components/Project/Project";
@@ -65,13 +17,97 @@ import Project from "../components/Project/Project";
 
 
 
+
+
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    const [availableSkills, setAvailableSkills] = useState([]);
+    const [selectedSkill, setSelectedSkill] = useState("all");
+    const [loading, setloading] = useState(false)
+
+
+    // console.log("availableSkills", availableSkills)
+    // console.log("selectedSkill", selectedSkill)
+
+
+    const GetProjects = async () => {
+        try {
+            setloading(true);
+            const res = await fetch("https://backendportfolio-seven.vercel.app/api/projects");
+            const data = await res.json();
+
+            if (data.success) {
+                setProjects(data.projects);
+                const preferredSkills = ["next", "react", "js", "node", "asp.net", "typescript"];
+
+
+                const allSkills = data.projects.flatMap(p => p.skills);
+                const filteredSkills = [...new Set(allSkills)].filter(skill =>
+                    preferredSkills.includes(skill.trim().toLowerCase())
+                );
+                setAvailableSkills(filteredSkills);
+            } else {
+                toast.error("API response error:", data.message || "Unknown error");
+            }
+        } catch (err) {
+            toast.warning("Fetch error:", err);
+        } finally {
+            setloading(false);
+        }
+    }
+
+    useEffect(() => {
+        GetProjects();
+    }, []);
+
+    const filteredProjects =
+        selectedSkill === "all"
+            ? projects
+            : projects.filter(project => project.skills.includes(selectedSkill));
+
+    if (loading) {
+        return (
+            <div className="text-center flex justify-center items-center h-100">
+                <ScaleLoader color="#41c900" />
+            </div>
+        )
+    }
+
 
     return (
         <>
             <Heading title="Projects" />
             <motion.div className="projects py-5 lg:px-10 ">
-                <Project />
+                {!loading && (
+                    <div className="flex gap-2 flex-wrap mb-6  justify-center">
+                        <button
+                            className={`px-3 py-1 rounded-xl border cursor-pointer hover:bg-gray-400 hover:text-white ${selectedSkill === "all"
+                                ? "bg-green-600 text-white"
+                                : "bg-gray-200 text-black"
+                                }`}
+                            onClick={() => setSelectedSkill("all")}
+                        >
+                            All
+                        </button>
+
+                        {availableSkills.map((skill, i) => (
+                            <button
+                                key={i}
+                                className={`px-3 py-1 rounded-xl border cursor-pointer hover:bg-gray-400 hover:text-white ${selectedSkill === skill
+                                    ? "bg-green-600 text-white"
+                                    : "bg-gray-200 text-black"
+                                    }`}
+                                onClick={() => setSelectedSkill(skill)}
+                            >
+                                {skill}
+                            </button>
+                        ))}
+                    </div>
+                )}
+
+
+                <Project filteredProjects={filteredProjects} />
             </motion.div>
         </>
     );
